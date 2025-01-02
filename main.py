ab88ea6f6565ade186ea30c84628ad278cff0859
ENI3GMA7 <164993903+ENI3GMA7@users.noreply.github.com>
2025-01-02 12:08:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ app.include_router(menu.router, prefix="/api", tags=["Menu"])
 # Corrige o nome da variável __name__ e adiciona a porta padrão
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0")
