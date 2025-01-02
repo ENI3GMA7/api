d89207a4795dc07cf7323ec179db6a0bf1b3e680
ENI3GMA7 <164993903+ENI3GMA7@users.noreply.github.com>
2025-01-02 10:41:18 +0000

    Update database.py

--- fast/app/database.py
+++ fast/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:password@localhost:3306/reserva_restaurante"
+DATABASE_URL = "sqlite///banco.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
