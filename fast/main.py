2adb6295e3a37dfea7c9cece8bc875ca39427cba
ENI3GMA7 <164993903+ENI3GMA7@users.noreply.github.com>
2025-01-02 10:36:54 +0000

    Update main.py

--- fast/main.py
+++ fast/main.py
@@ -8,3 +8,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(menu.router, prefix="/api", tags=["Menu"])
+
+if __name__"__main__":
+  import uvicorn
+    uvicorn.run(app,host=0.0.0.0)
