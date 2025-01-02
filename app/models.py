b14e34994d9fb96b988f51ca1ca33e3cb65ae21e
ENI3GMA7 <164993903+ENI3GMA7@users.noreply.github.com>
2025-01-02 12:19:45 +0000

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Float
 from app.database import Base
 
 class MenuItem(Base):
-    _tablename_ = "menu_items"
+    __tablename__ = "menu_items"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
