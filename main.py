31dbf79055b0c5e10c9ff1307cec31fc0f470b92
ENI3GMA7 <164993903+ENI3GMA7@users.noreply.github.com>
2025-01-16 11:51:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from app.routes import menu
 from app.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 # Cria as tabelas no banco de dados
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Lista de origens permitidas
+    allow_credentials=True,  # Permitir envio de cookies ou credenciais
+    allow_methods=["*"],  # Permitir todos os métodos HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
 # Inclui o roteador para as rotas do menu
 app.include_router(menu.router, prefix="/api", tags=["Menu"])
 
